ec7c020d6a25525fe291af29c2bd9609c2785e1b
yu-johnathan <yuj@carleton.edu>
2025-04-21 22:44:23 -0500

    afsnkdmlaskdmas

--- ProductionCode/data.py
+++ ProductionCode/data.py
@@ -1,5 +1,5 @@
-'''The purpose of this module is to import and process datasets from various streaming services.
-It creates a 3D list of media entries and a dictionary indexed by title for easy access.'''
+"""The purpose of this module is to import and process datasets from various streaming services.
+It creates a 3D list of media entries and a dictionary indexed by title for easy access."""
 
 import csv
 from collections import OrderedDict
@@ -19,46 +19,65 @@ LISTED_IN = 10
 DESCRIPTION = 11
 STREAMING_SERVICE = 12
 
+
 class Data:
+    """A class to represent the dataset of movies and shows from various streaming services"""
+
     def __init__(self):
         self.media_list = import_all_datasets_to_list()
         self.media_dict = create_media_dict_by_title(self.media_list)
 
     def get_media_list(self):
+        """
+        Returns the list containing data for all shows/movies from all streaming services.
+        [<service>, <row>, <column>]
+        """
         return self.media_list
 
     def get_media_dict(self):
+        """
+        Returns the dictionary containing data for all shows/movies from all streaming services.
+        {<title>: <Media object>}
+        """
         return self.media_dict
 
     def print_media_list(self):
+        """
+        Prints the entire media list, including the data from all 4 streaming services.
+        """
         for service in self.media_list:
             print(f"STREAMING SERVICE: {service}\n")
             for entry in service:
                 print(f"{entry}\n")
 
+
 class Media:
-    """ A class to represent a single entry in the dataset """
+    """
+    A class to represent a single movie or show entry from the dataset 
+    and all of its associated information.
+    """
     def __init__(self, entry):
         _fill_empty_fields(entry)
-        self.title=entry[TITLE]
-        self.show_id=entry[SHOW_ID]
-        self.media_type=entry[MEDIA_TYPE]
-        self.director=_make_set(entry[DIRECTOR])
-        self.cast=_make_set(entry[CAST])
-        self.country=_make_set(entry[COUNTRY])
-        self.date_added=entry[DATE_ADDED]
-        self.release_year=entry[RELEASE_YEAR]
-        self.rating=entry[RATING]
-        self.duration=entry[DURATION]
-        self.listed_in=_make_set(entry[LISTED_IN])
-        self.description=entry[DESCRIPTION]
-        self.streaming_service={entry[STREAMING_SERVICE]}
+        self.title = entry[TITLE]
+        self.show_id = entry[SHOW_ID]
+        self.media_type = entry[MEDIA_TYPE]
+        self.director = _make_set(entry[DIRECTOR])
+        self.cast = _make_set(entry[CAST])
+        self.country = _make_set(entry[COUNTRY])
+        self.date_added = entry[DATE_ADDED]
+        self.release_year = entry[RELEASE_YEAR]
+        self.rating = entry[RATING]
+        self.duration = entry[DURATION]
+        self.listed_in = _make_set(entry[LISTED_IN])
+        self.description = entry[DESCRIPTION]
+        self.streaming_service = {entry[STREAMING_SERVICE]}
+
 
 def import_all_datasets_to_list(
     netflix_dataset="Data/netflix_titles.csv",
     amazon_dataset="Data/amazon_prime_titles.csv",
     disney_dataset="Data/disney_plus_titles.csv",
-    hulu_dataset="Data/hulu_titles.csv"
+    hulu_dataset="Data/hulu_titles.csv",
 ):
     netflix_data = []
     amazon_prime_data = []
@@ -70,22 +89,19 @@ def import_all_datasets_to_list(
     import_dataset_to_list(disney_dataset, disney_plus_data, "Disney+")
     import_dataset_to_list(hulu_dataset, hulu_data, "Hulu")
 
-    media_list = [
-        netflix_data,
-        amazon_prime_data,
-        disney_plus_data,
-        hulu_data
-    ]
+    media_list = [netflix_data, amazon_prime_data, disney_plus_data, hulu_data]
     return media_list
 
+
 def import_dataset_to_list(dataset, data, streaming_service_name):
-    with open(dataset,encoding="utf-8") as csvfile:
+    with open(dataset, encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
         for row in reader:
             row.append(streaming_service_name)
             data.append(row)
- 
+
+
 def create_media_dict_by_title(data):
     media_dict = {}
     for streaming_service in data:
@@ -96,8 +112,9 @@ def create_media_dict_by_title(data):
     media_dict = _sort_dict_by_key(media_dict)
     return media_dict
 
+
 def _add_media_to_dict_by_title(media, media_dict):
-    """ Adds a movie or series to the media_by_title dictionary """
+    """Adds a movie or series to the media_by_title dictionary"""
     title = media.title
 
     if title not in media_dict:
@@ -106,22 +123,26 @@ def _add_media_to_dict_by_title(media, media_dict):
         for service in media_dict[title].streaming_service:
             media_dict[title].streaming_service.add(service)
 
+
 def _fill_empty_fields(entry):
     for i in range(len(entry)):
         if entry[i] == "":
             entry[i] = "Unspecified"
 
+
 def _make_set(string):
-    """ Helper function to convert a string to a set """
+    """Helper function to convert a string to a set"""
     return set(string.split(", "))
 
+
 def _sort_dict_by_key(d):
-    """ Sorts a dictionary by its keys """
+    """Sorts a dictionary by its keys"""
     return OrderedDict(sorted(d.items()))
+
 
 def main():
     data = Data()
-    #data.print_media_list()
+    # data.print_media_list()
 
 
 if __name__ == "__main__":
