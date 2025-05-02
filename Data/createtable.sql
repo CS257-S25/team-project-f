2b15df3c65fa8a65c42f8a366e9d0e589bbb14c5
grumdahla <grumdahla@carleton.edu>
2025-05-02 13:58:07 -0400

    added createtable.sql

---
+++ Data/createtable.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS amazon_prime_data;
+CREATE TABLE amazon_prime_data (
+    show_id text,
+    media_type text,
+    title text,
+    director text,
+    media_cast text,
+    country text,
+    date_added text,
+    release_year text,
+    rating text,
+    duration text,
+    category text,
+    media_description text
+);
