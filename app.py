d8efca1da3dd8eac1b31ba03479528caa565458e
Maria Fernanda del Granado <delgranadom@carleton.edu>
2025-05-12 19:22:05 -0400

    style edits

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ def homepage():
     return (
         """
         <h1>Welcome to StreamSearch</h1></br></br>
-        StreamSearch helps you find movies and TV shows based on actor names, categories, and release years.</br></br>
+        StreamSearch helps you find movies and TV shows based on 
+        actor names, categories, and release years.</br></br>
         <b>How to Use StreamSearch:</b></br>
         * <b>Actor</b>: Enter the name of an actor to find movies or shows they appear in. </br>
         * <b>Category</b>: Search by genre or category (e.g., Comedy, Action, Drama). </br>
-        * <b>Year</b>: Filter results to show movies or shows released on or after a specified year.</br></br>
+        * <b>Year</b>: Filter results to show movies or shows released on or after 
+        a specified year.</br></br>
         
         <b>Try these examples:</b></br>
         [URL]/actor/Emma Stone</br>
@@ -81,20 +83,20 @@ def page_not_found(e):
     """
     print(e)
     return (
-        "<h1>Error 404 - Wrong Format</h1></br></br>"
-        "<p>Oops! The page you requested does not exist.</p>"
-        "<p>Make sure your URL follows one of these formats:</p>"
+    "<h1>Error 404 - Wrong Format</h1></br></br>"
+    "<p>Oops! The page you requested does not exist.</p>"
+    "<p>Make sure your URL follows one of these formats:</p>"
 
-        "[URL]/actor/Actor Name</br>"
-        "[URL]/category/Category Name</br>"
-        "[URL]/year/Year</br></br>"
-        
-        "<b>Here are some example URLs:</br></br>"
-        "[URL]/actor/Emma Stone</br>"
-        "[URL]/category/Comedy</br>"
-        "[URL]/year/2010</br></br>"
-        "<p><a href=\"/\">Click to return to the homepage</a></p>", 
-        404)
+    "[URL]/actor/Actor Name</br>"
+    "[URL]/category/Category Name</br>"
+    "[URL]/year/Year</br></br>"
+    
+    "<b>Here are some example URLs:</br></br>"
+    "[URL]/actor/Emma Stone</br>"
+    "[URL]/category/Comedy</br>"
+    "[URL]/year/2010</br></br>"
+    "<p><a href=\"/\">Click to return to the homepage</a></p>", 
+    404)
 
 @app.errorhandler(500)
 def python_bug(e):
