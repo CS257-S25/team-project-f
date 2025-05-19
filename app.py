2277687285a051651be3ee822272531f07ddf0a6
Maria Fernanda del Granado <delgranadom@carleton.edu>
2025-05-19 15:55:13 -0400

    edited app.py route filter/results

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def search_by_year(year):
              or a message indicating no results were found.
     """
     try:
-        results = db.get_movies_later_than(year)
+        results = db.get_movies_later_than(year-1)
         if not results:
             return f"No movies found released after {year}."
         return "</br></br>".join(f"<b>{row[0]}</b> ({row[3]}): {row[1]}" for row in results)
@@ -81,10 +81,20 @@ def search_by_category(category):
 
 
 @app.route('/filter', methods=['GET'])
-def genre_form():
-    """Renders the genre selection form with dynamic dropdown."""
+def filter_form():
+    """Renders genre selection form with dynamic dropdown."""
     categories = db.get_all_categories()
     return render_template('filter.html', categories=categories)
+
+           ###### MEANT FOR SEARCHBAR WORK IN PROGRESS ######
+
+@app.route('/filter/search', methods=['GET'])
+def searchbar_results():
+    title = request.args.get('title', '')
+    titles = db.get_movie_by_title('title')
+    return render_template('index_html', catgeories=categories)
+
+           ###### MEANT FOR SEARCHBAR WORK IN PROGRESS ######
 
 @app.route('/filter/results', methods=['GET'])
 def filter_results():
@@ -92,24 +102,50 @@ def filter_results():
     category = request.args.get('category', '')
     actor = request.args.get('actor', '')
     year = request.args.get('year', '')
+
+    #just category search
     if category != ''and actor=='' and year=='':
         results = db.get_movies_by_category(category)
         return render_template('filter_results.html', category=category, results=results)
 
+    #just actor search
     if category ==''and actor!='' and year=='': 
         results = db.get_movie_titles_by_actor(actor)
         print("Results actor search:", results)
         print(type(results))
-        return render_template('actor_results.html', actor=actor, results=results)
+        return render_template('filter_results.html', actor=actor, results=results)
     
+    #just year search
     if category ==''and actor=='' and year!='': 
-        results = db.get_movies_later_than(year)
+        results = db.get_movies_later_than(str(int(year)-1))
         print("Results year search:")
         print(type(results))
-        return render_template('year_results.html', year=year, results=results)
+        return render_template('filter_results.html', year=year, results=results)
 
+    #Category, actor and year search (ALL THREE)
     if category !=''and actor!='' and year!='': 
-        results = db.get_3_filter_media(actor, year, category)
+        results = db.get_3_filter_media(actor, str(int(year)-1), category)
+        print("Results all filters search:", results)
+        print(type(results))
+        return render_template('filter_results.html', actor = actor, year=year, category = category, results=results)
+
+    #Category and actor search
+    if category !=''and actor!='' and year=='': 
+        results = db.get_3_filter_media(actor, 0, category)
+        print("Results all filters search:", results)
+        print(type(results))
+        return render_template('filter_results.html', actor = actor, year=year, category = category, results=results)
+
+    #Category and  year search
+    if category !=''and actor=='' and year!='': 
+        results = db.get_3_filter_media(actor, str(int(year)-1), category)
+        print("Results all filters search:", results)
+        print(type(results))
+        return render_template('filter_results.html', actor = actor, year=year, category = category, results=results)
+
+    # Actor and year search
+    if category ==''and actor!='' and year!='': 
+        results = db.get_3_filter_media(actor, str(int(year)-1), category)
         print("Results all filters search:", results)
         print(type(results))
         return render_template('filter_results.html', actor = actor, year=year, category = category, results=results)
@@ -173,4 +209,4 @@ def cause_500():
     raise RuntimeError("Test exception to trigger 500 error")
 
 if __name__ == "__main__":
-    app.run(port=9403)
+    app.run(port=5002)
